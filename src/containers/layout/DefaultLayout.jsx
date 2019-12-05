22f7f8241ef4e04080a5472dec5eac3aaa7ce053
Andre Botta <andre.botta@outcomex.com.au>
2019-12-05 08:50:41 +0000

    refactor (DefaultLayout.jsx): update active navitem based on scroll position

--- src/containers/layout/DefaultLayout.jsx
+++ src/containers/layout/DefaultLayout.jsx
@@ -21,10 +21,10 @@ import ausFlag from "../../assets/images/australia.svg";
 const DefaultLayout = props => {
 
     const [isOpen, setIsOpen] = useState(false);
+    const [activeNavitem, setActiveNavitem] = useState("");
     const prevScrollY = useRef(0);
 
     const [goingUp, setGoingUp] = useState(true);
-    const [transparentNavbar, setTransparentNavbar] = useState(false);
 
     const { t, i18n } = useTranslation();
 
@@ -38,11 +38,7 @@ const DefaultLayout = props => {
     useEffect(() => {
         const scrollHandler = event => {
             const currentScrollY = window.scrollY;
-            // if (currentScrollY < 50) {
-            //     setTransparentNavbar(true)
-            // } else {
-            //     setTransparentNavbar(false)
-            // }
+
             if (prevScrollY.current < currentScrollY && goingUp) {
                 setGoingUp(false);
             }
@@ -50,18 +46,38 @@ const DefaultLayout = props => {
                 setGoingUp(true);
             }
 
+            const sections = document.querySelectorAll(".custom-section");
+            // console.log(sections)
+
+            let sectionID = ""
+            sections.forEach(section => {
+                if (window.pageYOffset >= section.offsetTop - 100 &&
+                    window.pageYOffset <= section.offsetTop + section.offsetHeight - 100) {
+
+                    sectionID = section.id
+
+                }
+            });
+
+            setActiveNavitem(sectionID)
+
             prevScrollY.current = currentScrollY;
+
+
         };
         window.addEventListener("scroll", scrollHandler, true);
         return () => {
             window.removeEventListener("scroll", scrollHandler, true);
         };
-    }, [props, t, goingUp, transparentNavbar]);
+    }, [props, t, goingUp, activeNavitem]);
 
+    const isActive = (sectionID) => {
+        return activeNavitem === sectionID;
+    }
 
     return (
         <div className="app">
-            <div className={"botta-navbar " + (transparentNavbar ? "transparent " : (goingUp ? "show" : "hide"))}>
+            <div className={"botta-navbar " + (goingUp ? "show" : "hide")}>
                 <Navbar color="light" light expand="md" fixed={"top"}>
                     <NavbarBrand>
                         <div className="botta-navbar-brand">
@@ -87,31 +103,20 @@ const DefaultLayout = props => {
                     <NavbarToggler onClick={toggle} />
                     <Collapse isOpen={isOpen} navbar>
                         <Nav className="ml-auto" navbar>
-                            {/* <NavItem>
-                                <NavLink href="https://github.com/alcbotta" className="default-icon-size"
-                                    rel="noopener noreferrer" target="_blank"><i className="fa fa-github fa-lg" aria-hidden="true"></i></NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink href="https://medium.com/@alcbotta" className="default-icon-size"
-                                    rel="noopener noreferrer" target="_blank"><i className="fa fa-medium fa-lg" aria-hidden="true"></i></NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink href="https://www.linkedin.com/in/andrebotta/" className="default-icon-size"
-                                    rel="noopener noreferrer" target="_blank"><i className="fa fa-linkedin fa-lg" aria-hidden="true"></i></NavLink>
-                            </NavItem> */}
-                            <NavItem>
+
+                            <NavItem active={isActive("about")}>
                                 <NavLink href="#about">{t("navbar.aboutme")}</NavLink>
                             </NavItem>
-                            <NavItem>
-                                <NavLink href="#services">{t("navbar.projects")}</NavLink>
+                            <NavItem active={isActive("projects")}>
+                                <NavLink href="#projects">{t("navbar.projects")}</NavLink>
                             </NavItem>
-                            <NavItem>
-                                <NavLink href="#portifio">{t("navbar.education")}</NavLink>
+                            <NavItem active={isActive("education")}>
+                                <NavLink href="#education">{t("navbar.education")}</NavLink>
                             </NavItem>
-                            <NavItem>
-                                <NavLink href="#founders">{t("navbar.certifications")}</NavLink>
+                            <NavItem active={isActive("certifications")}>
+                                <NavLink href="#certifications">{t("navbar.certifications")}</NavLink>
                             </NavItem>
-                            <NavItem>
+                            <NavItem active={isActive("contact")}>
                                 <NavLink href="#contact">{t("navbar.contactme")}</NavLink>
                             </NavItem>
                             <NavItem>
@@ -173,8 +178,8 @@ const DefaultLayout = props => {
 
             </section>
             <section
-                // ref="aboutMe" 
-                id="about-me">
+                className="custom-section"
+                id="about">
                 <div className="title">
                     <h2>ABOUT ME </h2>
                 </div>
@@ -277,7 +282,7 @@ const DefaultLayout = props => {
                 </Container>
             </section>
 
-            <section className="custom-section" id="contact-me">
+            <section className="custom-section" id="contact">
                 <div className="container">
                     <div className="title">
                         <h2>CONTACT ME</h2>
@@ -305,143 +310,7 @@ const DefaultLayout = props => {
                     </Container>
                 </div>
             </section>
-            {/* 
-            
 
-            <div>
-                <section ref="aboutMe" className="custom-section" id="about-me">
-                    <div className="title">
-                        <h2>ABOUT ME </h2>
-                    </div>
-                    <div className="spacer2">  <br /></div>
-
-                    <Container>
-                        <Row>
-                            <Col>
-                                <div className="content">
-                                    TODO
-                  </div>
-                            </Col>
-                            <Col>
-                                TODO
-                </Col>
-                        </Row>
-                    </Container>
-
-                </section>
-
-                <section className="custom-section" id="projects">
-                    <div className="title">
-                        <h2>PROJECTS AND INTERESTS</h2>
-                    </div>
-                    <div className="spacer2">  <br /></div>
-
-                    <Container>
-                        <Row>
-                            <Col>
-                                <h3>RECENT PROJECTS</h3>
-                                <ul>
-                                    <li>
-
-                                    </li>
-                                    <li>TODO</li>
-                                    <li>TODO</li>
-                                </ul>
-                            </Col>
-                            <Col>
-                                <h3>RECENT PROJECTS</h3>
-                                TODO
-
-                </Col>
-                        </Row>
-                    </Container>
-                </section>
-
-                <section className="custom-section" id="education">
-                    <div className="title">
-                        <h2>EDUCATION</h2>
-                    </div>
-                    <div className="spacer2">  <br /></div>
-
-                    <Container>
-                        <Row>
-                            <Col>
-                                <Media>
-                                    <Media left >
-                                        <img width={64} height={64} src={utfprLogo} alt="thumbnail" />
-
-                                    </Media>
-                                    <Media body>
-                                        <Media heading>
-                                            Federal University of Technology – Parana
-        </Media>
-                                        Computer Engineering - Five years undergraduate course
-      </Media>
-                                </Media>
-                            </Col>
-                            <Col>
-                                <Media>
-                                    <Media left >
-                                        <img width={64} height={64} src={uobLogo} alt="thumbnail" />
-                                    </Media>
-                                    <Media body>
-                                        <Media heading>
-                                            University of Birmingham
-        </Media>
-                                        Computer Science/Software Engineering - One year undergraduate study abroad
-      </Media>
-                                </Media>
-                            </Col>
-                        </Row>
-
-                    </Container>
-                </section>
-
-
-                <section className="custom-section" id="certifications">
-                    <div className="title">
-                        <h2>CERTIFICATIONS</h2>
-                    </div>
-                    <div className="spacer2">  <br /></div>
-
-                    <Container>
-                        <Row>
-                            <Col>
-                                <Certifications />
-                            </Col>
-                        </Row>
-                    </Container>
-                </section>
-
-                <section className="custom-section" id="contact-me">
-                    <div className="container">
-                        <div className="title">
-                            <h2>CONTACT ME</h2>
-                        </div>
-                        <div className="spacer2">  <br /></div>
-                        <Container>
-                            <Form action="https://formspree.io/alcbotta@gmail.com" method="POST" name="sentMessage" id="contactForm" noValidate>
-
-                                <FormGroup>
-                                    <Label>Name</Label>
-                                    <Input required={true} type="text" name="contact-name" id="contact-name" placeholder="Your name" />
-                                </FormGroup>
-                                <FormGroup>
-                                    <Label>Email</Label>
-                                    <Input required type="email" name="contact-email" id="contact-email" placeholder="Your email" />
-                                </FormGroup>
-                                <FormGroup>
-                                    <Label>Message</Label>
-                                    <Input required type="textarea" name="contact-message" id="contact-message" placeholder="Your message" />
-                                </FormGroup>
-                                <Button>Send</Button>
-
-                            </Form>
-
-                        </Container>
-                    </div>
-                </section>
-            </div> */}
         </div>
     );
 
