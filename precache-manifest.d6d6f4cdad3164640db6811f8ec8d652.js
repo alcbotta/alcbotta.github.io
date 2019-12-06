a5f46156e714509c6c57c608c118a8bb8435b918
alcbotta <alcbotta@gmail.com>
2019-12-06 23:50:32 +0000

    commit c7776518f02a626e9007147994072dcc2248041d Author: Andre Botta <alcbotta@gmail.com> Date:   Fri Dec 6 23:48:55 2019 +0000
        refactor (devcontainer.json): update postCreateCommand with right git config

---
+++ precache-manifest.d6d6f4cdad3164640db6811f8ec8d652.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a10f215183d7f35605bb071bdd71c1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbc4c1fc37982b3f63b1",
+    "url": "/static/css/2.9720967d.chunk.css"
+  },
+  {
+    "revision": "55b21f168575cea94af3",
+    "url": "/static/css/main.d49a7b28.chunk.css"
+  },
+  {
+    "revision": "fbc4c1fc37982b3f63b1",
+    "url": "/static/js/2.7810da4b.chunk.js"
+  },
+  {
+    "revision": "55b21f168575cea94af3",
+    "url": "/static/js/main.d6cac146.chunk.js"
+  },
+  {
+    "revision": "4c40b94f0748754179e8",
+    "url": "/static/js/runtime-main.bbc06ef5.js"
+  },
+  {
+    "revision": "cab2eac60acd879c5b5d0b3c70ec913b",
+    "url": "/static/media/australia.cab2eac6.svg"
+  },
+  {
+    "revision": "a102e5631626f71ddbc427003063dc0a",
+    "url": "/static/media/br.a102e563.svg"
+  },
+  {
+    "revision": "49dcdfd1e2a8148e9fd911d748a60664",
+    "url": "/static/media/perfil.49dcdfd1.jpg"
+  }
+]);
